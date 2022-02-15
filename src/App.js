ccc9bd1a37051a03c488cf02b9a75ff1281f385e
Jeremiah Newman <jeremiah.n.a.newman@gmail.com>
2022-02-15 10:42:13 -0500

    Created Home, About, Products and CatchAll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Product from "./Product";
+import CatchAll from "./CatchAll";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React Router!</h1>
+      <Link to='product/1'>Product1</Link>
+      <br/>
+      <Link to='product/2'>Product2</Link>
+      <br/>
+      <Link to='product/3'>Product3</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path='product/:id' element={<Product />} />
+        
+        <Route path='*' element={<CatchAll />} />
+
+      </Routes>
     </div>
   );
 }
